2471ea463e3456c49f773d6b73d0758ea153e119
abcdana <abcdanakim@gmail.com>
2021-07-06 21:01:46 +0900

    Add Programmers Q.EvenOrOdd

---
+++ src/com/test/algo/programmers/level1/KthNumber.java
@@ -0,0 +1,13 @@
+package com.test.algo.programmers.level1;
+
+public class KthNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        return answer;
+    }
+}
