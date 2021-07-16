4f029948d4876c1f32ae08a6335c43580a5728d6
abcdana <74163296+abcdana@users.noreply.github.com>
2021-07-16 23:26:25 +0900

    Add programmers level1 KthNumber

--- src/com/test/algo/programmers/level1/KthNumber.java
+++ src/com/test/algo/programmers/level1/KthNumber.java
@@ -3,7 +3,7 @@ package com.test.algo.programmers.level1;
 public class KthNumber {
 
     public static void main(String[] args) {
-
+        int num = 0;
     }
 
     public int[] solution(int[] array, int[][] commands) {
