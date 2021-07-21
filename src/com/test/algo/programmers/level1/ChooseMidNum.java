974f39c100d2ba0ac6b0771e0cfbb7bd66cafa3a
abcdana <abcdanakim@gmail.com>
2021-07-21 20:57:25 +0900

    Add Programmers Q.ChooseMidNum

---
+++ src/com/test/algo/programmers/level1/ChooseMidNum.java
@@ -0,0 +1,21 @@
+package com.test.algo.programmers.level1;
+
+public class ChooseMidNum {
+
+    public static String solution(String s) {
+        String answer = "";
+
+        for (int i=(s.length()-1)/2; i<s.length()-1; i++){
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abcde";
+
+        System.out.println(solution(s));
+
+    }
+}
