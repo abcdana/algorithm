e084b50a4e937d7a1ec3bcf27c74eaec2996b274
abcdana <abcdanakim@gmail.com>
2021-07-24 09:44:56 +0900

    Add Programmers Lvl.1 - Q.AddMatrix

---
+++ src/com/test/algo/programmers/level1/AddMatrix.java
@@ -0,0 +1,16 @@
+package com.test.algo.programmers.level1;
+
+public class AddMatrix {
+
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{1,2},{2,3}};
+        int[][] arr2 = {{3,4},{5,6}};
+
+
+    }
+}
