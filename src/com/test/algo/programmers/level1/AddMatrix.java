84705c8310f4672be82398213b9cf70c15c7e348
abcdana <abcdanakim@gmail.com>
2021-07-26 14:43:55 +0900

    Add Programmers Lvl.1 - Q.AddMatrix

--- src/com/test/algo/programmers/level1/AddMatrix.java
+++ src/com/test/algo/programmers/level1/AddMatrix.java
@@ -3,16 +3,22 @@ package com.test.algo.programmers.level1;
 public class AddMatrix {
 
     public static int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = {};
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        int i, j;
+
+        for (i = 0; i < arr1.length; i++) {
+            for (j = 0; j < arr1[0].length; j++) {
+                System.out.printf("i = %d , j = %d", i, j);
+                answer[i][j] = (arr1[i][j] + arr2[i][j]);
+            }
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
         int[][] arr1 = {{1,2},{2,3}};
-
-
         int[][] arr2 = {{3,4},{5,6}};
-
 
         System.out.println(solution(arr1, arr2));
     }
