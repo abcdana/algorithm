cbb5349154e7e429b78a79e4d24f1c1018e459ea
abcdana <abcdanakim@gmail.com>
2021-07-28 23:20:10 +0900

    Add Programmers Lvl.1 - Q.DeleteMinValue

--- src/com/test/algo/programmers/level1/DeleteMinValue.java
+++ src/com/test/algo/programmers/level1/DeleteMinValue.java
@@ -1,56 +1,52 @@
 package com.test.algo.programmers.level1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class DeleteMinValue {
 
     public static int[] solution(int[] arr) {
-        int[] answer = new int[0];
-        int min = 0;
-        int j = 0;
+        int[] answer = {};
 
-        if (arr.length == 1) {
+        if (arr.length == 1){
             answer = new int[1];
             answer[0] = -1;
-            return answer;
 
         } else {
+            answer = new int[arr.length - 1];
+            int min = 0, index = 0;
 
-            answer = new int[arr.length-1];
-
-            for(int i = 0; i < arr.length-1; i++){
-                min = (arr[i] < arr[i+1]) ? arr[i] : arr[i+1];
-
-                if (min != arr[i]){
-
-                    answer[j] = arr[i];
-
-                    System.out.printf("arr[%d] = %d\n", i, arr[i]);
-                    System.out.printf("answer[%d] = %d\n", j, answer[j]);
-
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[i] <= arr[j]) {
+                        min = arr[i];
+                    }
                 }
-                j++;
             }
+            System.out.println("min : " + min);
 
-            System.out.println("------------------------");
-            for(int n : answer) System.out.printf("answer[%d] = %d\n", j, answer[j-1]);
-
-           return answer;
+            for (int i = 0; i < arr.length-1; i++){
+                if (arr[i] != min){
+                    answer[index] = arr[i];
+                    index++;
+                }
+            }
         }
 
-        //System.out.println(min);
-            //return answer;
 
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {4, 3, 2, 1};
-        int[] answer = new int[0];
-
-        solution(arr);
-
-        for (int i=0; i<answer.length-1; i++){
+        for (int i=0; i<answer.length; i++){
             System.out.println(answer[i]);
         }
 
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,5,7,3,2,6};
+
+        System.out.println(solution(arr));
 
     }
 
