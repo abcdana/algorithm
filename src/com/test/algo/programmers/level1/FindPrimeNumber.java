5027e58fa498f50f7584829d03b03b0a1340b439
abcdana <74163296+abcdana@users.noreply.github.com>
2021-07-29 22:17:41 +0900

    Add programmers level1 Q.FindPrimeNumber

---
+++ src/com/test/algo/programmers/level1/FindPrimeNumber.java
@@ -0,0 +1,16 @@
+package com.test.algo.programmers.level1;
+
+public class FindPrimeNumber {
+
+    public static int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 0;
+        System.out.println(solution(n));
+
+    }
+}
