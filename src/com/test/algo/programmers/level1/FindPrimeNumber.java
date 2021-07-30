f5d881addca4112255344ce8990e90b3f3070991
abcdana <abcdanakim@gmail.com>
2021-07-30 12:02:52 +0900

    Add Programmers Lvl.1 - Q.FindPrimeNumber

--- src/com/test/algo/programmers/level1/FindPrimeNumber.java
+++ src/com/test/algo/programmers/level1/FindPrimeNumber.java
@@ -4,12 +4,16 @@ public class FindPrimeNumber {
 
     public static int solution(int n) {
         int answer = 0;
+
+        for(int i = 1; i < n; i++){
+            if(n % i == 0) answer = i;
+        }
         return answer;
     }
 
     public static void main(String[] args) {
 
-        int n = 0;
+        int n = 10;
         System.out.println(solution(n));
 
     }
