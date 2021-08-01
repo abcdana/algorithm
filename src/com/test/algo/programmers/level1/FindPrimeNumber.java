a37d4b1157e696ad54b5aa6c061db6a144962fbe
abcdana <abcdanakim@gmail.com>
2021-08-01 20:41:38 +0900

    Add Programmers Lvl.1 - Q.FindPrimeNumber

--- src/com/test/algo/programmers/level1/FindPrimeNumber.java
+++ src/com/test/algo/programmers/level1/FindPrimeNumber.java
@@ -1,19 +1,31 @@
 package com.test.algo.programmers.level1;
 
+import java.util.ArrayList;
+
 public class FindPrimeNumber {
 
     public static int solution(int n) {
         int answer = 0;
+        ArrayList<Integer> primeNum = new ArrayList();
 
-        for(int i = 1; i < n; i++){
-            if(n % i == 0) answer = i;
+        for (int i = 0; i <= n; i++) {
+            primeNum.add(i);
         }
+
+        for (int i = 2; i <= n; i++){
+            if (primeNum.get(i) == 0) continue;
+            for (int j = 2*i; j <= n; j+=i) primeNum.set(j, 0);
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (primeNum.get(i) != 0 && primeNum.get(i) != 1) answer++;
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
 
-        int b = 10;
         int n = 10;
         System.out.println(solution(n));
 
