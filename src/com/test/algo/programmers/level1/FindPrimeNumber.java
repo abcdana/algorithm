1a307f95c4f9ace4ca353084a788982d66b026ad
abcdana <abcdanakim@gmail.com>
2021-08-03 19:36:08 +0900

    Add Programmers Lvl.1 - Q.FindPrimeNumber

--- src/com/test/algo/programmers/level1/FindPrimeNumber.java
+++ src/com/test/algo/programmers/level1/FindPrimeNumber.java
@@ -6,23 +6,26 @@ public class FindPrimeNumber {
 
     public static int solution(int n) {
         int answer = 0;
+        int[] arr = new int[n+1];
 
-        boolean[] primeNum = new boolean[n+1];
-        primeNum[1] = true;
-
-        for(int i = 2; i <= n; i++) {
-            for(int j = 2; i*j <= n; j++) {
-                primeNum[i*j] = true;
+        for(int i=0;i<=n;i++){
+            arr[i]=i;
+        }//0=1 n-1 =n이 된다
+        arr[1]=0;
+        for(int i=2;i<=n;i++){//2부터 계산해줌
+            if(arr[i]==0)continue;
+            for(int j=i+i;j<=n;j+=i){
+                arr[j]=0;
             }
         }
 
-        for (int i = 2; i <= n; i++){
-            if (primeNum[i] == true) answer++;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                answer++;
+            }
         }
-
-        System.out.println(answer);
-
-        return answer - 1;
+        return answer;
     }
 
     public static void main(String[] args) {
