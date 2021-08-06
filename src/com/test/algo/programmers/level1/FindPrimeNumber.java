99598d723384e1eff5f01ac6fcb74f6729ac171f
abcdana <abcdanakim@gmail.com>
2021-08-06 21:28:28 +0900

    Add Programmers Lvl.1 - Q.FindPrimeNumber

--- src/com/test/algo/programmers/level1/FindPrimeNumber.java
+++ src/com/test/algo/programmers/level1/FindPrimeNumber.java
@@ -19,11 +19,6 @@ public class FindPrimeNumber {
                 arr[j]=0;
             }
         }
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-                answer++;
-            }
-        }
 
 
         for(int i=0;i<arr.length;i++){
