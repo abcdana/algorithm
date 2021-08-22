b48c2140451e7ca10f74286e71f64970304ac2c1
abcdana <abcdanakim@gmail.com>
2021-08-22 23:00:41 +0900

    Programmers level1

--- src/com/test/algo/programmers/level1/AddAllDigit.java
+++ src/com/test/algo/programmers/level1/AddAllDigit.java
@@ -7,8 +7,6 @@ public class AddAllDigit {
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("Hello Java");
-        System.out.println("Hello Java");
-        System.out.println("Hello Java");
 
 
 
