5c8597cd854db3bee30fef8a98196611ddb11c00
abcdana <abcdanakim@gmail.com>
2021-09-02 22:36:11 +0900

    Programmers level1

--- src/com/test/algo/programmers/level1/FindPrimeNumber.java
+++ src/com/test/algo/programmers/level1/FindPrimeNumber.java
@@ -25,6 +25,7 @@ public class FindPrimeNumber {
         for(int i=0;i<arr.length;i++){
             if(arr[i]!=0){
                 answer++;
+                //
             }
         }
         return answer;
