d02cc9ab9590f2c5a704b20b1c5e1b3846ad97d2
danakim <abcdanakim@gmail.com>
2024-08-10 01:31:26 +0900

    feat: 11720 숫자의 합ㅎ

--- src/com/test/columbus/string_math/B11720.java
+++ src/com/test/columbus/string_math/B11720.java
@@ -1,4 +1,37 @@
 package com.test.columbus.string_math;
 
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class B11720 {
+
+    /*
+    문제
+    N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+
+    출력
+    입력으로 주어진 숫자 N개의 합을 출력한다.
+    */
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String numbers = br.readLine();
+        int sum = 0;
+
+        for (int i=0; i<N; i++){
+            sum += Integer.parseInt(numbers.substring(i,i+1));
+        }
+
+        System.out.print(sum);
+
+    }
 }
