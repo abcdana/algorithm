88e841cc5a62dc0362d4ac5d0307fb2dd6a3f363
danakim <abcdanakim@gmail.com>
2024-09-09 15:55:28 +0900

    feat: 1157 단어 공부

--- src/com/test/columbus/string_math/B1157.java
+++ src/com/test/columbus/string_math/B1157.java
@@ -1,9 +1,23 @@
 package com.test.columbus.string_math;
 
+import java.io.*;
 public class B1157 {
 
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String binary = br.readLine();
+
+        int decimal = Integer.parseInt(binary, 2);
+        String octal = Integer.toOctalString(decimal);
+
+        bw.write(octal);
+        bw.flush();
+        bw.close();
+        br.close();
+
 
     }
 }
