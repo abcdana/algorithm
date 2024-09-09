c9205c47898e6d381bff8f5f4961da4f855bcf2a
danakim <abcdanakim@gmail.com>
2024-09-09 15:55:58 +0900

    feat: 1929 에라토스테네스의 체

--- src/com/test/columbus/string_math/B1929.java
+++ src/com/test/columbus/string_math/B1929.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.*;
 import java.util.*;
 
-public class B1929 {
+public class    B1929 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
