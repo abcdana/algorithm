ed86fa08a72bf00b087ca204341144165521ac9b
Dana Joy <74163296+abcdana@users.noreply.github.com>
2024-11-19 09:56:12 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1292 ms, Memory: 111256 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,23 +1,25 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0; i < N; i++){
-            pq.offer(Integer.parseInt(br.readLine()));
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        while (!pq.isEmpty()) {
-            bw.write(pq.poll() + "\n");
+        Arrays.sort(arr);
+
+        for (int num : arr) {
+            sb.append(num).append("\n");
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 }
